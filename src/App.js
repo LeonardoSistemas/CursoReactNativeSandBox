8a10d5519e7ff781428129025ac464b43a8bf1d8
LeonardoSistemas <43937635+LeonardoSistemas@users.noreply.github.com>
2020-01-24 10:13:41 -0300

    Ajustes da calculadora

--- src/App.js
+++ src/App.js
@@ -14,12 +14,12 @@ const initialState = {
 class App extends Component {
   state = { ...initialState };
   addDigit = n => {
-    if (n === "." && this.state.displayValue.includes(".")) {
-      return;
-    }
     const clearDisplay =
       this.state.displayValue === "0" || this.state.clearDisplay;
 
+    if (n === "." && !clearDisplay && this.state.displayValue.includes(".")) {
+      return;
+    }
     const currentValue = clearDisplay ? "" : this.state.displayValue;
     const displayValue = currentValue + n;
     this.setState({ displayValue, clearDisplay: false });
@@ -46,7 +46,7 @@ class App extends Component {
       }
       values[1] = 0;
       this.setState({
-        displayValue: values[0],
+        displayValue: `${values[0]}`,
         operation: equals ? null : operation,
         current: equals ? 0 : 1,
         clearDisplay: !equals,
